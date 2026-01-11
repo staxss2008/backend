b16fe18fc62769e8ab5be524da81859bc4ce242f
staxss <tal_cfd@163.com>
2026-01-11 23:25:47 +0800

    修复编译错误 - 重写主应用类

--- src/main/java/com/vehicle/management/VehicleManagementApplication.java
+++ src/main/java/com/vehicle/management/VehicleManagementApplication.java
@@ -8,20 +8,16 @@ import org.mybatis.spring.annotation.MapperScan;
 
 /**
  * 派车管理系统主应用类
+ * 排除JPA自动配置,避免数据库连接冲突,只使用MyBatis-Plus
  */
 @SpringBootApplication(exclude = {
     org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
     org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration.class,
     org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
 })
-@MapperScan("com.vehicle.management.mapper")(exclude = {
-    org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
-    org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration.class,
-    org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
-})
 @MapperScan("com.vehicle.management.mapper")
 public class VehicleManagementApplication extends SpringBootServletInitializer {
-    
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(VehicleManagementApplication.class);
